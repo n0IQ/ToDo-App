c9df599224680e54a99009a5388799ac505e07ce
n0IQ <varmamohit122@gmail.com>
2023-05-19 13:35:20 +0530

    initial commit

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export const REACT_APP_API_KEY = "AIzaSyC8awwKrJP7lAC836vq8NtcDNjv2oHgcOc";
+export const REACT_APP_AUTHDOMAIN = "todo-app-4c2ed.firebaseapp.com";
+export const REACT_APP_PROJECT_ID = "todo-app-4c2ed";
+export const REACT_APP_STORAGE_BUCKET = "todo-app-4c2ed.appspot.com";
+export const REACT_APP_MESSAGING_SENDER_ID = "666536140546";
+export const REACT_APP_APP_ID = "1:666536140546:web:c84a3bb9c23c34c961ac1b";
